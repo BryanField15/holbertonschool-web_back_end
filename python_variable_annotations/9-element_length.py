24b10412fa8bcd7f46bb54c750ffb08c99c8b745
BryanField15 <6367@holbertonstudents.com>
2023-08-27 19:47:18 -0700

    task 9 solution

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -2,6 +2,7 @@
 """Module contains type-annotated function make_multiplier"""
 from typing import List, Tuple, Iterable, Sequence
 
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Function that returns a function that multiplies a float"""
     return [(i, len(i)) for i in lst]
