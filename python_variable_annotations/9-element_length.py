86b7b2acbace90c26eae8389e818070980720aca
BryanField15 <6367@holbertonstudents.com>
2023-08-27 19:50:26 -0700

    updated doc

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-"""Module contains type-annotated function make_multiplier"""
+"""Module contains type-annotated function element_lenght"""
 from typing import List, Tuple, Iterable, Sequence
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    """Function that returns a function that multiplies a float"""
+    """Function that returns an elements length"""
     return [(i, len(i)) for i in lst]
