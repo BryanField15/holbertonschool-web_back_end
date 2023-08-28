ac9a8bd357f5052b9ac3836155614dbe5d3fa740
BryanField15 <6367@holbertonstudents.com>
2023-08-27 19:03:30 -0700

    task 5 soltuion

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -2,6 +2,7 @@
 """Module contains type-annotated function sum_list"""
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
     """Function that sums a list of floats"""
     return sum(input_list)
