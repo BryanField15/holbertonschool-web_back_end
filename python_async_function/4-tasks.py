11f9fbb9d710d8b7dd518c72dd4de19c949290f6
BryanField15 <6367@holbertonstudents.com>
2023-08-28 20:39:43 -0700

    task 4 solution

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module that contains a basic asynchronous coroutine"""
+import asyncio
+from typing import List
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Async coroutine spawns wait_random n times with specified max_delay."""
+    list_of_all_delays = []
+    task_wait_random = __import__('0-basic_async_syntax').wait_random
+    for count in range(n):
+        delay = await task_wait_random(max_delay)
+        list_of_all_delays.append(delay)
+    return sorted(list_of_all_delays)
