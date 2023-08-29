fea04f10a74790fc547f5a8c041ee05d40e53c14
BryanField15 <6367@holbertonstudents.com>
2023-08-28 20:31:25 -0700

    task 3 solution

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -6,6 +6,6 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int):
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """Function that takes in an int and returns an asyncio.Task"""
     return asyncio.create_task(wait_random(max_delay))
