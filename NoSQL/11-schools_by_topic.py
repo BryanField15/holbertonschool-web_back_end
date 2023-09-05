00851755a4bfb07cad9a7e7a0e48b1c108cbfeee
root <4bryanfield@gmail.com>
2023-09-04 18:42:50 -0700

    task 11 solution

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Module that contains the schools_by_topic function"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """Function that returns the list of school having a specific topic"""
+    return list(mongo_collection.find({"topics": topic}))
+
+
+#    new_list = list(mongo_collection.find({"topics": {"$in": [topic]}}))
+                     
