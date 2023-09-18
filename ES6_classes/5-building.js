471ac49d1fabe81291f8526c85bf2951883162a2
BryanField15 <4bryanfield@gmail.com>
2023-09-18 10:52:40 +0000

    I dunno

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,18 +1,14 @@
 export default class Building {
   constructor(sqft) {
-    this.setSqft(sqft);
+    this._sqft = sqft;
 
     if (this.evacuationWarningMessage !== Building.prototype.evacuationWarningMessage) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+      this.evacuationWarningMessage();
     }
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  setSqft(sqft) {
-    this._sqft = sqft;
   }
 
   // eslint-disable-next-line class-methods-use-this
