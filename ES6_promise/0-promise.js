f1fe0d31a7812fc8156dfd167957ecd47272fe65
BryanField15 <4bryanfield@gmail.com>
2023-10-05 11:32:29 +1100

    removed unused variable

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,5 +1,5 @@
 function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     setTimeout(() => {
       resolve('Data from API'); // Resolving the promise after 2 seconds
     }, 2000);
