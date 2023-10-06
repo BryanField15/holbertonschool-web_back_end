abb909cf6c377a7a2b3223450e1c3183b30633a1
BryanField15 <4bryanfield@gmail.com>
2023-10-06 21:26:59 +1100

    Double hyphen

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -2,9 +2,13 @@ function cleanSet(set, startString) {
   if (startString === '' || startString === null || typeof startString !== 'string') {
     return '';
   }
+
   const result = [...set]
     .filter((value) => value.startsWith(startString))
-    .map((value) => value.slice(startString.length))
+    .map((value) => {
+      if (value === startString) return value.slice(startString.length);
+      return value.replace(startString, '');
+    })
     .join('-');
 
   return result;
