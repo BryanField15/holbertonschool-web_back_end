0793abd50dd23dc13e03095751af7163b7e7ae63
BryanField15 <4bryanfield@gmail.com>
2023-10-06 21:41:33 +1100

    lint

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -4,8 +4,8 @@ function cleanSet(set, startString) {
   }
 
   const result = [...set]
-    .filter(value => typeof value === 'string' && value.startsWith(startString))
-    .map(value => value.slice(startString.length))
+    .filter((value) => typeof value === 'string' && value.startsWith(startString))
+    .map((value) => value.slice(startString.length))
     .join('-');
 
   return result;
