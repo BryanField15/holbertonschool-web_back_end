15561736769a6d24cbce1ef634e1bbe675fddf3a
BryanField15 <4bryanfield@gmail.com>
2023-10-06 21:21:56 +1100

    Errors

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,10 @@
 function cleanSet(set, startString) {
-  if (!startString) return '';
-
+  if (startString === '' || startString === null || typeof startString !== 'string') {
+    return '';
+  }
   const result = [...set]
-    .filter(value => value.startsWith(startString))
-    .map(value => value.slice(startString.length))
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length))
     .join('-');
 
   return result;
