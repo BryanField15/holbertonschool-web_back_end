9c7c8457fad47bff8efdd099155626b79eaa126e
Bryan Field <4bryanfield@gmail.com>
2024-05-23 10:49:03 +1000

    Task 0 solution

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,3 +1,23 @@
 #!/usr/bin/python3
-""" BaseCaching module
-"""
+""" BaseCache module"""
+
+from base_caching import BaseCaching
+
+class BasicCache(BaseCaching):
+    """BasicCache class that inherits from BaseCaching"""
+
+    def __init__(self):
+        """Initialize cache dictionary"""
+        self.cache_data = {}
+
+    def put(self, key, item):
+        """Assigns the item value given the key"""
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+    
+    def get(self, key):
+        """Return a value given the key"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
